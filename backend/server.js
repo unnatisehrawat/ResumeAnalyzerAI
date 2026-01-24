d2532fad9df6234a2d4db44e9affdabc8bc2b30d
unknown <seharwatunnati@gmail.com>
2026-01-24 22:45:06 +0530

    updated port

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,6 @@ import app from "./app.js";
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server running on 0.0.0.0:${PORT}`);
+app.listen(PORT,  () => {
+  console.log(`Server running on :${PORT}`);
 });
